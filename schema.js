e686e56a989ace9fa203d0d9e1929668c6fcd400
Alex <alex@Alexs-MacBook-Pro.local>
2023-01-17 12:34:01 -0500

    Queries now work by mapping data from the

--- schema.js
+++ schema.js
@@ -1,19 +1,52 @@
 const {
     GraphQLSchema,
     GraphQLObjectType,
-    GraphQLString
-  } = require('graphql')
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull,
+  } = require('graphql');
+  const User = require('./models/User');
+  const user = [];
+
+const UserType = new GraphQLObjectType({
+  name: 'Users',
+  description: 'This lists all users in the database.',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    username: { type: new GraphQLNonNull(GraphQLString) },
+    accesstoken: { type: new GraphQLNonNull(GraphQLString) },
+    refreshtoken: {type: new GraphQLNonNull(GraphQLString) },
+    password: { type: new GraphQLNonNull(GraphQLString) },
+  })
+})
+
+const RootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root Query',
+  fields: () => ({
+    users: {
+      type: new GraphQLList(UserType),
+      description: 'List of all Users',
+      resolve: async () => {
+          await User.findAll()
+          .then (res => {
+              res.map((data) => {
+                user.push(data.dataValues)
+                console.log('Data in Query=> ',data.dataValues)
+              })
+          })
+          .catch(error => {
+              console.log(error)
+            })
+        return (user) }
+    }
+  })   
+})
 
 const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-      name: 'HelloAlexMan',
-      fields: () => ({
-        message: {
-          type: GraphQLString,
-          resolve: () => 'Hello World'
-        }
-      })
-    })
-  })
+  query: RootQueryType
+})
+
 
   module.exports = schema;
