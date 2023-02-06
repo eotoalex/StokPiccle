22b450ba01dcacd7abe0e5df6e9e5f4c4cd0f4f1
Alex <alex@Alexs-MacBook-Pro.local>
2023-02-05 21:43:40 -0500

    Added an SVG for the email login, now I need to transform the SVG once clicked to invoke the input attribute in order to log users in with the SVG added from Figma.

--- babel.config.js
+++ babel.config.js
@@ -2,22 +2,3 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
 };
 
-// const { getDefaultConfig } = require("expo/metro-config");
-
-// module.exports = (() => {
-//   const config = getDefaultConfig(__dirname);
-
-//   const { transformer, resolver } = config;
-
-//   config.transformer = {
-//     ...transformer,
-//     babelTransformerPath: require.resolve("react-native-svg-transformer"),
-//   };
-//   config.resolver = {
-//     ...resolver,
-//     assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-//     sourceExts: [...resolver.sourceExts, "svg"],
-//   };
-
-//   return config;
-// })();
